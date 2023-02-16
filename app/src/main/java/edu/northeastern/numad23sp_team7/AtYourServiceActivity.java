7c22fe4ab68a64c7e8c3fca113895fe44d129e70
Qiansha Meng <mengqianshasha@163.com>
2023-02-16 10:47:09 -0800

    add api client and Restaurant data model

--- app/src/main/java/edu/northeastern/numad23sp_team7/AtYourServiceActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team7/AtYourServiceActivity.java
@@ -3,8 +3,10 @@ package edu.northeastern.numad23sp_team7;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +20,8 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AtYourServiceActivity extends AppCompatActivity {
@@ -26,12 +30,29 @@ public class AtYourServiceActivity extends AppCompatActivity {
 
     private EditText mURLEditText;
     private TextView mTitleTextView;
+    private Button button;
+    private YelpApiClient apiClient;
+    private List<Restaurant> restaurants;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_at_your_service);
 
+        button = findViewById(R.id.search_button);
+        apiClient = new YelpApiClient();
+        restaurants = new ArrayList<>();
+        handler = new Handler();
+
+        button.setOnClickListener(view -> {
+            Thread newThread = new Thread(() -> {
+                restaurants = apiClient.getRestaurants("", "");
+                handler.post(() -> {
+                    // Populate the recycler view
+                });
+            });
+        });
     }
 
 }
---
+++ app/src/main/java/edu/northeastern/numad23sp_team7/YelpApiClient.java
@@ -0,0 +1,10 @@
+package edu.northeastern.numad23sp_team7;
+
+import java.util.List;
+
+public class YelpApiClient {
+
+    public List<Restaurant> getRestaurants(String searchTerm, String location) {
+        return null;
+    }
+}
