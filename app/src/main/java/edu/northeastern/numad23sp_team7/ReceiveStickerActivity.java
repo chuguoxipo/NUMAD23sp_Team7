0b3a2eb745a434e523d6fb350b6e6f5596af3a32
chuguoxipo <lingfeicrystal@gmail.com>
2023-03-04 21:05:53 +0800

    Implement send notification

---
+++ app/src/main/java/edu/northeastern/numad23sp_team7/ReceiveStickerActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad23sp_team7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceiveStickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_sticker);
+    }
+}
