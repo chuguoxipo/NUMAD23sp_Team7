d6d7e6322b2b2f99b8ff79e42481ec3f0c30f856
WeixinLiu618 <liu.weix@northeastern.edu>
2023-03-23 13:23:31 -0700

    add huskymarket packages and add preference manager

---
+++ app/src/main/java/edu/northeastern/numad23sp_team7/huskymarket/utils/Constants.java
@@ -0,0 +1,9 @@
+package edu.northeastern.numad23sp_team7.huskymarket.utils;
+
+public class Constants {
+    public static final String KEY_PREFERENCE_NAME = "huskyMarketPreference";
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_COLLECTION_PRODUCTS = "products";
+    public static final String KEY_COLLECTION_CHAT = "chat";
+
+}
