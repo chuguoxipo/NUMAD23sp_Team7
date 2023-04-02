06f878c75000ba50f9e718a1b14d0409d02a18ae
WeixinLiu618 <liu.weix@northeastern.edu>
2023-04-02 14:22:31 -0700

    add profile fragment functions and connect to user collection

--- app/src/main/java/edu/northeastern/numad23sp_team7/huskymarket/activities/ProfileFragment.java
+++ app/src/main/java/edu/northeastern/numad23sp_team7/huskymarket/activities/ProfileFragment.java
@@ -1,51 +1,167 @@
 package edu.northeastern.numad23sp_team7.huskymarket.activities;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import edu.northeastern.numad23sp_team7.databinding.FragmentProfileBinding;
+import edu.northeastern.numad23sp_team7.huskymarket.utils.Constants;
 import edu.northeastern.numad23sp_team7.huskymarket.utils.PreferenceManager;
 
 
 public class ProfileFragment extends Fragment {
 
-   private FragmentProfileBinding binding;
-   private FirebaseAuth mAuth;
-   private PreferenceManager preferenceManager;
+    private FragmentProfileBinding binding;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore database;
+    private PreferenceManager preferenceManager;
+
+    private static final String TAG = "profile fragment";
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mAuth = FirebaseAuth.getInstance();
+        database = FirebaseFirestore.getInstance();
         preferenceManager = new PreferenceManager(requireContext());
         // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(getLayoutInflater());
+        showUserInfo();
 
 
         binding.iconLogout.setOnClickListener(v -> {
-            if(mAuth.getCurrentUser() != null) {
-                mAuth.signOut();
-                preferenceManager.clear();
-                Intent intent = new Intent(getActivity(), HuskyLoginActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
-            }
+            logout();
         });
+
+        binding.iconEditProfileImage.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            pickImage.launch(intent);
+        });
+
+
 
         return binding.getRoot();
     }
+
+    private void showUserInfo() {
+        binding.imageProfile.setImageBitmap(decodeProfileImageString(preferenceManager.getString(Constants.KEY_PROFILE_IMAGE)));
+        binding.username.setText(preferenceManager.getString(Constants.KEY_USERNAME));
+        binding.email.setText(preferenceManager.getString(Constants.KEY_EMAIL));
+        binding.editableUsername.setText(preferenceManager.getString(Constants.KEY_USERNAME));
+        // TODO set posts, sold, earnings to real numbers
+    }
+
+
+    private void logout() {
+        if (mAuth.getCurrentUser() != null) {
+            mAuth.signOut();
+            preferenceManager.clear();
+            Intent intent = new Intent(getActivity(), HuskyLoginActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+
+
+
+
+    // string -> bitmap
+    private Bitmap decodeProfileImageString(String encodedImage) {
+        byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+
+
+    // bitmap -> string
+    private String getEncodedImage(Bitmap bitmap) {
+        int width = 150;
+        int height = bitmap.getHeight() * width / bitmap.getWidth();
+        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+    private final ActivityResultLauncher<Intent> pickImage = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    if (result.getData() != null) {
+                        Uri imageUri = result.getData().getData();
+                        try {
+                            InputStream inputStream = getActivity().getContentResolver().openInputStream(imageUri);
+                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                            binding.imageProfile.setImageBitmap(bitmap);
+                            String encodedImage = getEncodedImage(bitmap);
+                            preferenceManager.putString(Constants.KEY_PROFILE_IMAGE, encodedImage);
+                            changeProfileImageInDB(encodedImage);
+
+                            Log.d(TAG, "image: " + preferenceManager.getString(Constants.KEY_PROFILE_IMAGE));
+
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
+
+    private void changeProfileImageInDB(String encodedImage) {
+
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID))
+                .update(Constants.KEY_PROFILE_IMAGE, encodedImage)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        showToast("Successfully change profile image!");
+                        Log.d(TAG, "DocumentSnapshot successfully updated!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        showToast("Fail to change profile image!");
+                        Log.w(TAG, "Error updating document", e);
+                    }
+                });
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
